b957b705339ce9594909ca452b07b32c898bf93c
Tam Pham <tampham47@live.com>
2016-08-29 22:21:42 +0700

    add url into channel

--- models/Channel.js
+++ models/Channel.js
@@ -14,7 +14,7 @@ var Channel = new keystone.List('Channel');
 
 Channel.add({
 	_user: { type: Types.Relationship, ref: 'User', index: true },
-	// url: { type: Types.Text, required: true, index: true },
+	url: { type: Types.Text, required: true, index: true },
 	name: { type: Types.Text, required: true, index: true },
 	state: { type: Types.Select, options: 'public, private', default: 'public', index: true },
 	brief: { type: Types.Html, wysiwyg: true, height: 150 },
