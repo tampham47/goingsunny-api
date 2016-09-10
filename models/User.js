ff1ca77a6c46dee20f745a180908098c618deb62
Tam Pham <tampham47@live.com>
2016-09-10 07:32:42 +0700

    dont edit fields that returned from 3rd party

--- models/User.js
+++ models/User.js
@@ -14,12 +14,12 @@ User.add({
 	email: { type: Types.Email, initial: true, required: true, index: true },
 	password: { type: Types.Password, initial: true, required: true },
 	username: { type: String },
-	provider: { type: String },
-	providerId: { type: String },
-	profileUrl: { type: String },
-	avatar: { type: String },
-	accessToken: { type: String },
-	refreshToken: { type: String },
+	provider: { type: String, noedit: true },
+	providerId: { type: String, noedit: true },
+	profileUrl: { type: String, noedit: true },
+	avatar: { type: String, noedit: true },
+	accessToken: { type: String, noedit: true },
+	refreshToken: { type: String, noedit: true },
 	createdAt: { type: Types.Datetime, default: Date.now, noedit: true },
 
 	/* Additional Information */
