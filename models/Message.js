3b76343d6708a53c573889927e425e83ae4a9b54
Tam Pham <tampham47@live.com>
2016-11-15 08:00:02 +0700

    update model of message

--- models/Message.js
+++ models/Message.js
@@ -18,6 +18,7 @@ Message.add({
 	_user: { type: Types.Relationship, ref: 'User', index: true },
 	_channel: { type: Types.Relationship, ref: 'Channel', index: true },
 	content: { type: Types.Textarea, initial: true, required: true },
+	channelUrl: { type: String },
 	type: { type: String },
 	createdAt: { type: Types.Datetime, default: Date.now, noedit: true }
 });
@@ -25,7 +26,6 @@ Message.add({
 //Add
 Message.schema.add({ 
 	authUser: keystone.mongoose.Schema.Types.Mixed,
-	channelUrl: String
 });
 
 /**
@@ -37,5 +37,5 @@ Message.schema.add({
  * Registration
  */
 
-Message.defaultColumns = '_channel, _user, content, createdAt';
+Message.defaultColumns = 'channelUrl, _user, content, createdAt';
 Message.register();
