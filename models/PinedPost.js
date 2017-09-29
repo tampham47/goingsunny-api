c1ec3e13d2f46262283c58356d1fda11b510c1d3
Tam Pham <tampham47@live.com>
2017-09-29 22:05:34 +0700

    d

---
+++ models/PinedPost.js
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * PinedPost Model
+ * ==========
+ */
+
+var PinedPost = new keystone.List('PinedPost', {
+  defaultSort: '-createdAt'
+});
+
+PinedPost.add({
+  title: { type: Types.Text, required: true, initial: true },
+  url: { type: Types.Url, required: true, initial: true },
+  type: { type: Types.Select, options: 'link, pdf, youtube', default: 'link', initial: true },
+  state: { type: Types.Select, options: 'public, archive', default: 'public' },
+  description: { type: Types.Textarea, height: 150 },
+  createdAt: { type: Types.Datetime, default: Date.now, noedit: true }
+});
+
+PinedPost.defaultColumns = 'name, url, _user, createdAt';
+PinedPost.register();
