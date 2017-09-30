2be8a5307782b4cff74a44a95202a05e89645aef
Tam Pham <tampham47@live.com>
2017-09-30 20:17:02 +0700

    d

--- models/Session.js
+++ models/Session.js
@@ -17,8 +17,8 @@ var Session = new keystone.List('Session', {
 Session.add({
   _user: { type: Types.Relationship, ref: 'User', index: true, initial: true },
   sessionName: { type: Types.Text, initial: true },
-  roomName: { type: Types.Text, default: '' },
-  isConfirmed: { type: Boolean, default: false },
+  roomName: { type: Types.Text, default: '', initial: true },
+  isConfirmed: { type: Boolean, default: false, initial: true },
   createdAt: { type: Types.Datetime, default: Date.now, noedit: true },
 });
 
@@ -26,13 +26,17 @@ Session.schema.pre('save', function(next) {
   if (!this._user) {
     return next(new Error('User is required.'));
   }
+
+  if (!this.isNew) {
+    return next(); // allow the old one free to update
+  }
+
+  // only check for the new one
   keystone.list('Session').model.find({
     _user: this._user,
     sessionName: this.sessionName,
     roomName: '',
   }).exec(function(err, r) {
-    console.log('data', this);
-    console.log('data', r);
     if (err) {
       return next(new Error(err));
     }
