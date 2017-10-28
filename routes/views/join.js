ca81fa56abb0dd1d1550ea4be5e1dbdc4bfb3d61
Tam Pham <tampham47@live.com>
2017-10-28 13:54:03 +0700

    normalize content from chatfuel

--- routes/views/join.js
+++ routes/views/join.js
@@ -2,6 +2,21 @@ var keystone = require('keystone');
 var Session = keystone.list('Session');
 
 exports = module.exports = function(req, res) {
-  console.log('query', req.query);
+  const query = req.query;
+  const data = {
+    messengerId: query['messenger user id'],
+    firstName: query['first name'],
+    lastName: query['last name'],
+    gender: query['gender'],
+    profileUrl: query['profile pic url'],
+    locale: query['locale'],
+    ref: query['ref'],
+    timezone: query['timezone'],
+    chatfuelId: query['chatfuel user id'],
+    lastVisitedBlockId: query['last visited block id'],
+    lastVisitedBlockName: query['last visited block name'],
+    subscriptions: query['rss and search subscriptions'],
+  };
+  console.log('data', query, data);
   res.send({ message: 'ok', });
 };
