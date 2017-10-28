bbf1e6b150f2f34689b11dbcab8e9ed5622b1388
Tam Pham <tampham47@live.com>
2017-10-28 16:21:17 +0700

    d

--- routes/views/join.js
+++ routes/views/join.js
@@ -1,5 +1,29 @@
 var keystone = require('keystone');
-var Session = keystone.list('Session');
+var Session = keystone.list('Session').model;
+var moment = require('moment');
+var fetch = require('node-fetch');
+
+const botId = '59f28d26e4b0640c0cdc9930';
+const apiToken = 'mELtlMAHYqR0BvgEiMq8zVek3uYUK3OJMbtyrdNPTrQB9ndV0fM7lWTFZbM4MZvD';
+const blockFailId = '59f4498be4b0640c16ce4135';
+const blockSuccessId = '59f4414be4b0640c16a2b060';
+
+const sendSuccess = (messengerId) => {
+  return fetch(`https://api.chatfuel.com/bots/${botId}/users/${messengerId}/send?chatfuel_token=${apiToken}&chatfuel_block_id=${blockSuccessId}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  });
+};
+const sendFail = (messengerId) => {
+  return fetch(`https://api.chatfuel.com/bots/${botId}/users/${messengerId}/send?chatfuel_token=${apiToken}&chatfuel_block_id=${blockFailId}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  });
+};
 
 exports = module.exports = function(req, res) {
   const query = req.query;
@@ -20,6 +44,23 @@ exports = module.exports = function(req, res) {
     lastVisitedButtonName: query['last clicked button name'],
     lastUserFreeformInput: query['last user freeform input'],
   };
-  console.log('data', query, data);
-  res.send({ message: 'ok', });
+
+  const n = {
+    _messenger: data.messengerId,
+    sessionName: moment().format('YYYYMMDD'),
+  };
+
+  Session.create(n, function (err, newSession) {
+
+    if (err) {
+      sendFail(n._messenger);
+      res.send({
+        err: 'You are in queue already.',
+      });
+      return;
+    }
+
+    sendSuccess(n._messenger);
+    res.send({data: newSession});
+  });
 };
