41b9de47b5c5f6e90e0fc2e73fded33faf05287f
Tam Pham <tampham47@live.com>
2017-11-19 09:55:08 +0700

    d

--- routes/index.js
+++ routes/index.js
@@ -60,8 +60,17 @@ exports = module.exports = function(app) {
   restify.serve(router, keystone.mongoose.model('PinedPost'));
 
   restify.serve(router, keystone.mongoose.model('Subscribe'), {
+    preMiddleware: (req, res, next) => {
+      console.log('preMiddleware req', req.body);
+      next();
+    },
     preCreate: (req, res, next) => {
+      console.log('preCreate body', req.body);
       req.body._messenger = req.body._messenger || req.body['messenger user id'];
+      next();
+    },
+    onError: (err, req, res, next) => {
+      console.log('err', err);
       next();
     },
   });
