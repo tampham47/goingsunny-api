5c85f510996c3103120f9d36aee8de762af4479d
Tam Pham <tampham47@live.com>
2017-11-25 18:14:28 +0700

    Add oxford (#6)
    * d
    
    * d

--- keystone.js
+++ keystone.js
@@ -2,7 +2,7 @@
 // customising the .env file in your project's root folder.
 
 if (process.env.NODE_ENV === 'local') {
-	require('dotenv').load();
+  require('dotenv').load();
 }
 
 // Require keystone
@@ -15,29 +15,29 @@ var handlebars = require('express-handlebars');
 
 keystone.init({
 
-	'name': 'GoingsunnyDatacenter',
-	'brand': 'GoingsunnyApi',
-	
-	'sass': 'public',
-	'static': 'public',
-	'favicon': 'public/favicon.ico',
-	'views': 'templates/views',
-	'view engine': 'hbs',
-	
-	'custom engine': handlebars.create({
-		layoutsDir: 'templates/views/layouts',
-		partialsDir: 'templates/views/partials',
-		defaultLayout: 'default',
-		helpers: new require('./templates/views/helpers')(),
-		extname: '.hbs'
-	}).engine,
-	
-	'emails': 'templates/emails',
-	
-	'auto update': true,
-	'session': true,
-	'auth': true,
-	'user model': 'User'
+  'name': 'GoingsunnyDatacenter',
+  'brand': 'GoingsunnyApi',
+
+  'sass': 'public',
+  'static': 'public',
+  'favicon': 'public/favicon.ico',
+  'views': 'templates/views',
+  'view engine': 'hbs',
+
+  'custom engine': handlebars.create({
+    layoutsDir: 'templates/views/layouts',
+    partialsDir: 'templates/views/partials',
+    defaultLayout: 'default',
+    helpers: new require('./templates/views/helpers')(),
+    extname: '.hbs'
+  }).engine,
+
+  'emails': 'templates/emails',
+
+  'auto update': true,
+  'session': true,
+  'auth': true,
+  'user model': 'User'
 
 });
 
@@ -50,10 +50,10 @@ keystone.import('models');
 // for each request) should be added to ./routes/middleware.js
 
 keystone.set('locals', {
-	_: require('underscore'),
-	env: keystone.get('env'),
-	utils: keystone.utils,
-	editable: keystone.content.editable
+  _: require('underscore'),
+  env: keystone.get('env'),
+  utils: keystone.utils,
+  editable: keystone.content.editable
 });
 
 // Load your project's Routes
@@ -66,18 +66,18 @@ keystone.set('cors allow origin', true);
 // default email templates, you may remove them if you're using your own.
 
 keystone.set('email locals', {
-	logo_src: '/images/logo-email.gif',
-	logo_width: 194,
-	logo_height: 76,
-	theme: {
-		email_bg: '#f9f9f9',
-		link_color: '#2697de',
-		buttons: {
-			color: '#fff',
-			background_color: '#2697de',
-			border_color: '#1a7cb7'
-		}
-	}
+  logo_src: '/images/logo-email.gif',
+  logo_width: 194,
+  logo_height: 76,
+  theme: {
+    email_bg: '#f9f9f9',
+    link_color: '#2697de',
+    buttons: {
+      color: '#fff',
+      background_color: '#2697de',
+      border_color: '#1a7cb7'
+    }
+  }
 });
 
 // Setup replacement rules for emails, to automate the handling of differences
@@ -101,14 +101,16 @@ keystone.set('email tests', require('./routes/emails'));
 // Configure the navigation bar in Keystone's Admin UI
 
 keystone.set('nav', {
-	'lessons': ['lessons', 'lesson-craw-data'],
-	'sessions': 'sessions',
-	'users': 'users',
-	'notifications': ['notifications', 'user-notifications' ],
-	'channels': ['channels', 'messages', 'user-in-channels'],
-	'posts': ['posts', 'post-categories'],
-	'galleries': 'galleries',
-	'enquiries': 'enquiries'
+  'sessions': ['sessions', 'subscribes'],
+  'oxford': 'oxfords',
+  'pined-post': 'pined-posts',
+  'users': 'users',
+  'lessons': ['lessons', 'lesson-craw-data'],
+  'notifications': ['notifications', 'user-notifications' ],
+  'channels': ['channels', 'messages', 'user-in-channels'],
+  'posts': ['posts', 'post-categories'],
+  'galleries': 'galleries',
+  'enquiries': 'enquiries'
 });
 
 // Start Keystone to connect to your database and initialise the web server
