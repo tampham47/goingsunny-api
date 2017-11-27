4b5756d2f5e8a28ddb099888f3a059b5a6295af5
Tam Pham <tampham47@live.com>
2017-11-27 08:18:01 +0700

    d

--- routes/views/showtime.js
+++ routes/views/showtime.js
@@ -32,7 +32,8 @@ exports = module.exports = function (req, res) {
   if (currentHour >= 20) { // in time
     sendSuccess(messengerId);
   } else {
-    sendFail(messengerId);
+    sendSuccess(messengerId);
+    // sendFail(messengerId);
   }
 
   res.send({ data: 'ok' });
