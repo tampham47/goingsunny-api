0e7985733765cd29a25e3b523a4a1d075000392e
Tam Pham <tampham47@live.com>
2019-01-19 18:33:29 +0700

    update user model

--- keystone.js
+++ keystone.js
@@ -1,6 +1,5 @@
 // Simulate config options from your production environment by
 // customising the .env file in your project's root folder.
-
 if (process.env.NODE_ENV === 'local') {
   require('dotenv').load();
 }
@@ -12,9 +11,7 @@ var handlebars = require('express-handlebars');
 // Initialise Keystone with your project's configuration.
 // See http://keystonejs.com/guide/config for available options
 // and documentation.
-
 keystone.init({
-
   'name': 'GoingsunnyDatacenter',
   'brand': 'GoingsunnyApi',
 
@@ -33,22 +30,18 @@ keystone.init({
   }).engine,
 
   'emails': 'templates/emails',
-
   'auto update': true,
   'session': true,
   'auth': true,
   'user model': 'User'
-
 });
 
 // Load your project's Models
-
 keystone.import('models');
 
 // Setup common locals for your templates. The following are required for the
 // bundled templates and layouts. Any runtime locals (that should be set uniquely
 // for each request) should be added to ./routes/middleware.js
-
 keystone.set('locals', {
   _: require('underscore'),
   env: keystone.get('env'),
@@ -57,14 +50,11 @@ keystone.set('locals', {
 });
 
 // Load your project's Routes
-
 keystone.set('routes', require('./routes'));
 keystone.set('cors allow origin', true);
 
-
 // Setup common locals for your emails. The following are required by Keystone's
 // default email templates, you may remove them if you're using your own.
-
 keystone.set('email locals', {
   logo_src: '/images/logo-email.gif',
   logo_width: 194,
@@ -82,34 +72,30 @@ keystone.set('email locals', {
 
 // Setup replacement rules for emails, to automate the handling of differences
 // between development a production.
-
 // Be sure to update this rule to include your site's actual domain, and add
 // other rules your email templates require.
 
 // keystone.set('email rules', [{
-// 	find: '/images/',
-// 	replace: (keystone.get('env') == 'production') ? 'http://www.your-server.com/images/' : 'http://localhost:3000/images/'
+//   find: '/images/',
+//   replace: (keystone.get('env') == 'production') ? 'http://www.your-server.com/images/' : 'http://localhost:3000/images/'
 // }, {
-// 	find: '/keystone/',
-// 	replace: (keystone.get('env') == 'production') ? 'http://www.your-server.com/keystone/' : 'http://localhost:3000/keystone/'
+//   find: '/keystone/',
+//   replace: (keystone.get('env') == 'production') ? 'http://www.your-server.com/keystone/' : 'http://localhost:3000/keystone/'
 // }]);
 
 // Load your project's email test routes
-
 keystone.set('email tests', require('./routes/emails'));
 
 // Configure the navigation bar in Keystone's Admin UI
-
 keystone.set('nav', {
   'hacknao': ['kessays', 'kunits', 'kvocabs'],
+  'users': 'users',
   'howdy': ['topics', 'entries', 'events', 'blogs', 'videos'],
   'goingsunny': ['howdy-lessons', 'sessions', 'subscribes'],
   'oxford': 'oxfords',
   'pined-post': 'pined-posts',
-  'users': 'users',
   'lessons': ['lessons', 'lesson-craw-data'],
 });
 
 // Start Keystone to connect to your database and initialise the web server
-
 keystone.start();
