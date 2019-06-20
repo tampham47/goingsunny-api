40c0d95ea6494c0398d9db86cf2b0371f762b9bd
Tam Pham <tampham47@live.com>
2019-06-20 11:51:48 +0700

    add five more units

---
+++ updates/0.0.2-v-u6.js
@@ -0,0 +1,28 @@
+exports.create = {
+  KVocab: [
+    { unitName: 'hkb6', word: 'rich', type: 'adj', meaning: 'giàu', unit: '5d09b4cd7aaa6b49d7db8f1e' },
+    { unitName: 'hkb6', word: 'cheap', type: 'adj', meaning: 'rẻ', unit: '5d09b4cd7aaa6b49d7db8f1e' },
+    { unitName: 'hkb6', word: 'price', type: 'noun', meaning: 'giá tiền', unit: '5d09b4cd7aaa6b49d7db8f1e' },
+    { unitName: 'hkb6', word: 'rent', type: 'noun', meaning: 'tiền thuê nhà', unit: '5d09b4cd7aaa6b49d7db8f1e' },
+    { unitName: 'hkb6', word: 'expensive', type: 'adj', meaning: 'đắt', unit: '5d09b4cd7aaa6b49d7db8f1e' },
+    { unitName: 'hkb6', word: 'manage', type: 'verb', meaning: 'quản lý', unit: '5d09b4cd7aaa6b49d7db8f1e' },
+    { unitName: 'hkb6', word: 'spend', type: 'verb', meaning: 'tiêu tiền', unit: '5d09b4cd7aaa6b49d7db8f1e' },
+    { unitName: 'hkb6', word: 'save', type: 'verb', meaning: 'tiết kiệm', unit: '5d09b4cd7aaa6b49d7db8f1e' },
+    { unitName: 'hkb6', word: 'wasteful', type: 'adj', meaning: 'phí phạm', unit: '5d09b4cd7aaa6b49d7db8f1e' },
+    { unitName: 'hkb6', word: 'borrow', type: 'verb', meaning: 'mượn', unit: '5d09b4cd7aaa6b49d7db8f1e' },
+    { unitName: 'hkb6', word: 'fee', type: 'noun', meaning: 'lệ phí', unit: '5d09b4cd7aaa6b49d7db8f1e' },
+    { unitName: 'hkb6', word: 'owe', type: 'verb', meaning: 'nợ', unit: '5d09b4cd7aaa6b49d7db8f1e' },
+    { unitName: 'hkb6', word: 'broke', type: 'adj', meaning: 'cháy túi', unit: '5d09b4cd7aaa6b49d7db8f1e' },
+    { unitName: 'hkb6', word: 'cost', type: 'noun', meaning: 'chi phí', unit: '5d09b4cd7aaa6b49d7db8f1e' },
+    { unitName: 'hkb6', word: 'lend', type: 'verb', meaning: 'cho vay', unit: '5d09b4cd7aaa6b49d7db8f1e' },
+    { unitName: 'hkb6', word: 'note', type: 'noun', meaning: 'tờ tiền', unit: '5d09b4cd7aaa6b49d7db8f1e' },
+    { unitName: 'hkb6', word: 'expense', type: 'noun', meaning: 'khoản chi tiêu', unit: '5d09b4cd7aaa6b49d7db8f1e' },
+    { unitName: 'hkb6', word: 'due', type: 'adj', meaning: 'đến hạn', unit: '5d09b4cd7aaa6b49d7db8f1e' },
+    { unitName: 'hkb6', word: 'bill', type: 'noun', meaning: 'hoá đơn', unit: '5d09b4cd7aaa6b49d7db8f1e' },
+    { unitName: 'hkb6', word: 'reasonable', type: 'adj', meaning: 'phải chăng', unit: '5d09b4cd7aaa6b49d7db8f1e' },
+    { unitName: 'hkb6', word: 'luxury', type: 'noun', meaning: 'điều xa hoa', unit: '5d09b4cd7aaa6b49d7db8f1e' },
+    { unitName: 'hkb6', word: 'generous', type: 'adj', meaning: 'hào phóng', unit: '5d09b4cd7aaa6b49d7db8f1e' },
+    { unitName: 'hkb6', word: 'raise', type: 'verb', meaning: 'tăng', unit: '5d09b4cd7aaa6b49d7db8f1e' },
+    { unitName: 'hkb6', word: 'decrease', type: 'verb', meaning: 'giảm', unit: '5d09b4cd7aaa6b49d7db8f1e' },
+  ]
+};
