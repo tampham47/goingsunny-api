3fd9797900c176e46b3220f9cee81d92e251542c
Tam Pham <tampham47@live.com>
2019-06-20 17:42:01 +0700

    add unit 31-35

---
+++ updates/0.0.2-v-u33.js
@@ -0,0 +1,28 @@
+exports.create = {
+  KVocab: [
+    { unitName: 'hkb33', word: 'celebrity', type: 'noun', meaning: 'người nổi tiếng', unit: '5d09b95b0805b64d5d33fc8d' },
+    { unitName: 'hkb33', word: 'private', type: 'adj', meaning: 'riêng tư', unit: '5d09b95b0805b64d5d33fc8d' },
+    { unitName: 'hkb33', word: 'paparazzi', type: 'noun', meaning: 'thợ săn ảnh', unit: '5d09b95b0805b64d5d33fc8d' },
+    { unitName: 'hkb33', word: 'charity', type: 'noun', meaning: 'tổ chức từ thiện', unit: '5d09b95b0805b64d5d33fc8d' },
+    { unitName: 'hkb33', word: 'lucky', type: 'adj', meaning: 'may mắn', unit: '5d09b95b0805b64d5d33fc8d' },
+    { unitName: 'hkb33', word: 'golden', type: 'adj', meaning: 'làm bằng vàng', unit: '5d09b95b0805b64d5d33fc8d' },
+    { unitName: 'hkb33', word: 'gate', type: 'noun', meaning: 'cổng', unit: '5d09b95b0805b64d5d33fc8d' },
+    { unitName: 'hkb33', word: 'shiny', type: 'adj', meaning: 'bóng loáng', unit: '5d09b95b0805b64d5d33fc8d' },
+    { unitName: 'hkb33', word: 'fancy', type: 'adj', meaning: 'sang chảnh', unit: '5d09b95b0805b64d5d33fc8d' },
+    { unitName: 'hkb33', word: 'immigration', type: 'noun', meaning: 'thủ tục nhập cảnh', unit: '5d09b95b0805b64d5d33fc8d' },
+    { unitName: 'hkb33', word: 'refuse', type: 'verb', meaning: 'từ chối', unit: '5d09b95b0805b64d5d33fc8d' },
+    { unitName: 'hkb33', word: 'enter', type: 'verb', meaning: 'đi vào', unit: '5d09b95b0805b64d5d33fc8d' },
+    { unitName: 'hkb33', word: 'interested', type: 'adj', meaning: 'có hứng thú', unit: '5d09b95b0805b64d5d33fc8d' },
+    { unitName: 'hkb33', word: 'fan', type: 'noun', meaning: 'người hâm mộ', unit: '5d09b95b0805b64d5d33fc8d' },
+    { unitName: 'hkb33', word: 'personal', type: 'adj', meaning: 'việc riêng tư', unit: '5d09b95b0805b64d5d33fc8d' },
+    { unitName: 'hkb33', word: 'scream', type: 'verb', meaning: 'thét', unit: '5d09b95b0805b64d5d33fc8d' },
+    { unitName: 'hkb33', word: 'copy', type: 'verb', meaning: 'bắt chước', unit: '5d09b95b0805b64d5d33fc8d' },
+    { unitName: 'hkb33', word: 'star', type: 'noun', meaning: 'ngôi sao', unit: '5d09b95b0805b64d5d33fc8d' },
+    { unitName: 'hkb33', word: 'model', type: 'noun', meaning: 'người mẫu', unit: '5d09b95b0805b64d5d33fc8d' },
+    { unitName: 'hkb33', word: 'actress', type: 'noun', meaning: 'nữ diễn viên', unit: '5d09b95b0805b64d5d33fc8d' },
+    { unitName: 'hkb33', word: 'comedy', type: 'noun', meaning: 'hài kịch', unit: '5d09b95b0805b64d5d33fc8d' },
+    { unitName: 'hkb33', word: 'drama', type: 'noun', meaning: 'tác phẩm kịch', unit: '5d09b95b0805b64d5d33fc8d' },
+    { unitName: 'hkb33', word: 'cartoon', type: 'noun', meaning: 'phim hoạt hình', unit: '5d09b95b0805b64d5d33fc8d' },
+    { unitName: 'hkb33', word: 'film', type: 'noun', meaning: 'bộ phim', unit: '5d09b95b0805b64d5d33fc8d' },
+  ]
+};
